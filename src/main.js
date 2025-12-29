0a938938267f0f39d9407778e6b0814c8dbe4708
abberdeen <abdurahim.khudoyberdiev@gmail.com>
2025-12-29 18:12:44 +0500

    x

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ import { Client, Databases, Query } from 'node-appwrite';
 export default async ({ req, res, log, error }) => {
   try {
     const client = new Client()
-      .setEndpoint(process.env.APPWRITE_FUNCTION_API_ENDPOINT)
-      .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
+      .setEndpoint(process.env.APPWRITE_ENDPOINT)
+      .setProject(process.env.APPWRITE_PROJECT_ID)
       .setKey(process.env.APPWRITE_FUNCTION_API_KEY);
 
     const databases = new Databases(client);
